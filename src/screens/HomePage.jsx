7ea7be4171a12935ed0ba7794fbc09271d04559d
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-27 13:10:24 +0300

    update homepage page

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Hero from "../components/HomeInd/Hero";
+import Companies from "../components/Reusable/Companies";
+import WhatWeOffer from "../components/HomeInd/WhatWeOffer";
+import Benefits from "../components/HomeInd/Benefit";
+import FAQ from "../components/HomeInd/FAQ";
+import Testimonial from "../components/HomeInd/Testimonials";
+import RegisterToday from "../components/Reusable/RegisterToday";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <Hero />
+      <Companies />
+      <WhatWeOffer />
+      <Benefits />
+      <FAQ />
+      <Testimonial />
+      <RegisterToday />
     </div>
   );
 };
