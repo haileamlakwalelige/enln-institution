283c2aff28a523a47d4d898c573fe4c098cd35d2
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-27 14:20:16 +0300

    upate app file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import Header from "./components/Reusable/HeaderBefore";
 import HomePage from "./screens/HomePage";
 import Footer from "./components/layout/Footer";
+import CourseLine from "./components/courses/CourseLine";
+import SignUp from "./screens/SignUp";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="course-line" element={<CourseLine />} />
+          <Route path="signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
       <Footer />
