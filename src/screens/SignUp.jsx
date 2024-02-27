44d3a9dc2a8ee194dcfc50a5b91abb26bbc1c5ac
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-27 16:06:08 +0300

    finalize signup page

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import signup from "../assets/signup.svg";
 import google from "../assets/google.png";
 import facebook from "../assets/facebook.png";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [passShow, setPassShow] = useState(false);
+
   return (
     <div className="py-10 ">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4  px-2 sm:px-4 md:px-12 lg:px-32 lg:gap-10 justify-center items-center">
@@ -16,41 +19,54 @@ const SignUp = () => {
           />
         </div>
         <div className="flex flex-col justify-center items-center">
-          <p className="text-4xl py-2 font-bold amir text-primary ">Sign Up</p>
+          <p className="text-20px md:text-[24px] lg:text-[28px] xl:text-[32px]  py-2 font-bold merb  text-primary ">
+            Sign Up
+          </p>
           <form className="flex flex-col">
             <input
               type="text"
               placeholder="Full Name"
-              className="rounded-xl border-primary border-2 my-2 max-w-[300px] min-w-[250px] amir"
+              className="rounded-md border-primary border-[1px] my-2 max-w-[300px] min-w-[250px] amir"
             />
             <input
               type="text"
               placeholder="Email"
-              className="rounded-xl border-primary border-2 my-2 max-w-[300px] min-w-[250px] amir"
+              className="rounded-md border-primary border-[1px] my-2 max-w-[300px] min-w-[250px] amir"
             />
-            <input
-              type="text"
-              placeholder="Password"
-              className="rounded-xl border-primary border-2 my-2 max-w-[300px] min-w-[250px] amir"
-            />
+            <div className="rounded-xl border-primary border-2 my-2 max-w-[300px] min-w-[250px] amir z-20 p-1 overflow-x-hidden flex justify-start items-start">
+              <input
+                type={passShow ? "text" : "password"}
+                placeholder="Password"
+                className="border-none outline-none focus:outline-none  z-10 rounded max-w-[300px] min-w-[250px]"
+              />
+              <div className="pt-2 pr-4" onClick={() => setPassShow(!passShow)}>
+                {passShow ? <FaRegEye /> : <FaRegEyeSlash />}
+              </div>
+            </div>
             <p className="px-3 py-3 text-black text-lg font-medium">
               Between 8 and 72 character
             </p>
             <button
               type="submit"
-              className="bg-primary rounded-xl text-white font-semibold text-xl py-2 px-10 max-w-[300px] min-w-[250px] my-4"
+              className="bg-primary rounded-md text-white font-semibold text-xl py-2 px-10 max-w-[300px] min-w-[250px] my-4"
             >
               Sign Up
             </button>
-            <div className="bg-white rounded-xl flex text-lg  text-black font-semibold gap-4 justify-start items-start py-2 px-4 max-w-[300px] min-w-[250px] my-4 border-primary border-2">
+            <div className="bg-white rounded-md flex text-lg  text-black font-semibold gap-4 justify-start items-start py-2 px-4 max-w-[300px] min-w-[250px] my-4 border-primary border-[1px]">
               <img src={google} alt="" className="h-[25px] w-[25px]" />
               <button>Continue With Google</button>
             </div>
-            <div className="bg-white rounded-xl flex text-black font-semibold gap-4 justify-start items-start py-2 px-4 max-w-[300px] min-w-[250px] my-4 border-primary border-2">
+            <div className="bg-white rounded-md flex text-black font-semibold gap-4 justify-start items-start py-2 px-4 max-w-[300px] min-w-[250px] my-4 border-primary border-[1px]">
               <img src={facebook} alt="" className="h-[25px] w-[25px]" />
               <button className="text-lg">Continue With Facebook</button>
             </div>
           </form>
+          <div className="flex gap-3 text-black font-medium">
+            <p>Already have an account?</p>
+            <Link to="login" className="text-primary font-bold">
+              Log In
+            </Link>
+          </div>
         </div>
       </div>
     </div>
