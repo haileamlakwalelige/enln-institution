8bc3bd5e89c43b3b6fce7da1333266a5f6cdd6c6
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-27 10:27:26 +0300

    update tailwindcss components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/preline/dist/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        ["infinite-slider"]: "infiniteSlider 20s linear infinite",
+      },
+      keyframes: {
+        infiniteSlider: {
+          "0%": { transform: "translateX(0)" },
+          "100%": {
+            transform: "translateX(calc(-250px * 5))",
+          },
+        },
+      },
+    },
   },
-  plugins: [require("daisyui")],
-}
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#025464",
+          secondary: "#b3cbd0",
+          accent: "#001014",
+        },
+      },
+      "light",
+    ],
+  },
+  plugins: [
+    require("daisyui", "prettier-plugin-tailwindcss", "@tailwindcss/line-clamp", "preline/plugin"),
+  ],
+};
