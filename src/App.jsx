149d5af75bb325679f9f0dcf4eb7f126f3e23cee
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-27 10:27:42 +0300

    hide the headerbefore section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Reusable/HeaderBefore";
+// import Header from "./components/Reusable/HeaderBefore";
 import HomePage from "./screens/HomePage";
 
 const App = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
