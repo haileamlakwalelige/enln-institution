f7d8dd7a07ef3312bdd6aab59a7fe6a77dc8b91e
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-28 10:38:51 +0300

    update category section

---
+++ src/screens/Categories.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import FilterSidebar from '../components/categories/FilterSidebar';
+import AllCoursesList from '../components/categories/AllCoursesList';
+function Categories() {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top when component is mounted
+  }, []);
+
+  return (
+    <>
+      <div className="w-11/12">
+        {/* <GetYouStarted /> */}
+        <div className="grid-rows mt-20 grid w-full lg:grid-cols-[30%,70%]">
+          <div className="lg:ml-2">
+            <FilterSidebar />
+          </div>
+          <div className="ml-5 lg:ml-5">
+            <AllCoursesList />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Categories;
