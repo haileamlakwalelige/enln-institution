a345e188032bd1dd9e03538d6fdcfb67c4791b68
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-29 11:20:22 +0300

    update hero page

--- src/components/HomeOrg/Hero.jsx
+++ src/components/HomeOrg/Hero.jsx
@@ -1,88 +1,35 @@
-import React from 'react'
-import { motion } from "framer-motion";
-import { Link } from 'react-router-dom';
+import React from "react";
+import org from "../../assets/orga.png";
 
-function Hero() {
+const Hero = () => {
   return (
-    <div>
-      <div
-        className="justify-left relative hidden min-h-[80vh] items-center lg:flex"
-        style={{ backgroundImage: 'url("/orgH.jpg")', backgroundSize: "cover" }}
-      >
-        <div
-          className="absolute inset-0 bg-gradient-to-r from-primary to-transparent opacity-100 "
-          style={{ mixBlendMode: "multiply" }}
-        ></div>
-        <div className="z-10 ml-16 w-1/2 text-white">
-          <h1 className=" mb-5 hidden text-5xl font-bold text-white lg:block">
-            Empower individuals for enhanced opportunities <br />
-          </h1>
-          <p className="mb-3 mt-3 text-justify text-xl text-white">
-            Our organization provides cutting-edge skills, ensuring readiness
-            for diverse and thriving careers. Elevate your potential with our
-            targeted programs for today's job market demands.
-          </p>
-
-          {/* Call to action button */}
-          <div className=" w-1/2 space-x-2 ">
-            <Link to="/organizationform">
-            <motion.a
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              transition={{ type: "spring", stiffness: 400, damping: 17 }}
-              className="btn btn-outline btn-primary hidden bg-white lg:flex lg:items-center"
-            >
+    <div className="min-h-[60vh] bg-gray-300">
+      <div className="flex justify-center gap-0 flex-col-reverse lg:flex-row items-center px-2 sm:px-6 md:px-12 lg:px-32 pt-0 lg:pt-6 py-10 lg:py-0">
+        <div className="lg:w-1/2 w-full justify-center lg:justify-between items-center lg:items-start flex flex-col gap-10 md:gap-20 lg:gap-32">
+          <div>
+            <p className="text-primary text-[36px] md:text-[42px] lg:text-[45px]  nun font-extrabold">
+              Empowering Leaders, <br /> Transforming Communities
+            </p>
+            <p className="font-medium text-primary mer leading-8 mt-3">
+              Harness &nbsp; the &nbsp; strength &nbsp; of &nbsp; collaboration
+              &nbsp; and &nbsp; align <br /> yourself &nbsp; with &nbsp; a
+              &nbsp; network &nbsp; deeply &nbsp; committed &nbsp; to &nbsp; the{" "}
+              <br />
+              cultivation &nbsp; of &nbsp; healthier &nbsp; communities.
+            </p>
+          </div>
+          <div>
+            <button className="text-white bg-primary font-semibold text-xl py-2 px-10 rounded">
               Contact Sales
-            </motion.a></Link>
+            </button>
           </div>
         </div>
-      </div>
-      <div className="mx-auto   w-11/12  lg:hidden ">
-        {/* Mobile image */}
-        <div
-          className=" relative flex min-h-[50vh]  w-full items-center bg-cover "
-          style={{
-            backgroundImage: 'url("/orgH.jpg")',
-            //backgroundSize: "cover",
-            backgroundSize: "100% 100%",
-          }}
-        >
-          <div
-            className="absolute inset-0 bg-gradient-to-r from-primary to-transparent opacity-100"
-            style={{ mixBlendMode: "multiply" }}
-          ></div>
-        </div>
-        <div className=" w-full">
-          <h1 className="orgphoneherotext font-bold text-white ">
-            Empower individuals for enhanced opportunities
-            <br />
-          </h1>
-
-          {/* Hero description */}
-          <p className="heroinnertext mb-3 mt-3 text-justify text-white ">
-            Our organization provides cutting-edge skills, ensuring readiness
-            for diverse and thriving careers. Elevate your potential with our
-            targeted programs for today's job market demands.
-          </p>
-
-          {/* Call to action button */}
-          <div className=" w-full  space-x-2 ">
-            <Link to="/organizationform">
-              {" "}
-              <motion.a
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                className="buttontext  btn btn-primary mt-6 w-full rounded-lg border-none px-8 py-2 uppercase tracking-wider duration-300 lg:w-auto"
-              >
-                Contact Sales
-              </motion.a>
-            </Link>
-          </div>
+        <div className="w-full lg:w-1/2 flex justify-end items-start">
+          <img src={org} alt="" className="w-full h-full" />
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Hero
+export default Hero;
