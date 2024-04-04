07f8e1fd2dd9e2c1b091ea0c86005dc3fc37ca62
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-04 14:37:04 +0300

    finally update the course description page

--- src/screens/CourseDescription.jsx
+++ src/screens/CourseDescription.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import CourseHero from "../components/course description/CoursesHero";
+import CoursesHero from "../components/course description/CoursesHero";
+import WhatWeLearn from "../components/course description/WhatWeLearn";
+import SingleCard from "../components/course description/SingleCard";
+import CourseContent from "../components/course description/CourseContent";
+import CourseRequirements from "../components/course description/CourseRequirements";
+import DescriptionCourse from "../components/course description/DescriptionCourse";
+import CourseWho from "../components/course description/CourseWho";
 
 const CourseDescription = () => {
   return (
     <div className="mt-44">
-      <CourseHero />
+      <CoursesHero />
+      <div className="flex flex-col-reverse lg:flex-row justify-start items-start my-10 mx-2 sm:mx-4 md:mx-6 lg:mx-10 gap-10">
+        <div className="lg:w-3/4 flex flex-col justify-center items-center ">
+          <WhatWeLearn />
+          <CourseContent />
+          <CourseRequirements />
+          <DescriptionCourse />
+          <CourseWho />
+        </div>
+        <div className="lg:w-1/4">
+          <SingleCard />
+        </div>
+      </div>
     </div>
   );
 };
