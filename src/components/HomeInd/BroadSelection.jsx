94c617874bc6954ff1e566a6cdcc8663fbf9d482
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-11 15:56:16 +0300

    add broad selection section

---
+++ src/components/HomeInd/BroadSelection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import VerticalCardsTrying from "../try/VerticalCardsTrying";
+
+const BroadSelection = () => {
+  return (
+    <div>
+      <div className="px-2 sm:px-6 md:px-12 lg:px-32">
+        <h1 className="heading py-5 text-center lg:text-start">
+          Broad Selection of Courses
+        </h1>
+        <p className="text-[21px] font-semibold text-black merb py-3">
+          Expand your career opportunities with Nutrition Leadership
+        </p>
+        <p className="tex-black text-[16px] amir">
+          Enhance your career prospects by delving into Nutrition Leadership.
+          This program is designed to broaden your knowledge and skills in
+          nutrition, providing valuable insights into leadership within the
+          field. Elevate your career trajectory with a focus on nutrition
+          leadership.
+        </p>
+      </div>
+      <div>
+        <VerticalCardsTrying />
+      </div>
+    </div>
+  );
+};
+
+export default BroadSelection;
