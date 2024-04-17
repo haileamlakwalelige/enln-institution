7272482eb6b88056ff952e60582acd45ac7cdd2a
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-17 12:08:44 +0300

    update header top section for smaller device

--- src/components/Reusable/HeaderTop.jsx
+++ src/components/Reusable/HeaderTop.jsx
@@ -4,21 +4,30 @@ import { Link } from "react-router-dom";
 const HeaderTop = () => {
   return (
     <div>
-      <div className="hidden z-30 fixed top-0 w-screen md:grid grid-cols-1 xl:grid-cols-2 bg-black text-white px-2 py-4">
-        <div className="flex justify-between gap-4 px-10 lg:px-2 ">
+      <div className=" z-30 fixed top-0 w-screen md:grid grid-cols-1 xl:grid-cols-2 bg-black text-white px-2 py-4">
+        <div className="flex justify-between gap-4 md:px-10 lg:px-2 px-2 ">
           <Link to="/">
-            <p className="font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
+            <p className="hidden lg:flex font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
               For Individuals
+            </p>
+            <p className="lg:hidden font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
+              Individuals
             </p>
           </Link>
           <Link to="/org-home">
-            <p className="font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
+            <p className="hidden lg:flex font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
               For Organizations
+            </p>
+            <p className="flex lg:hidden font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
+              Organizations
             </p>
           </Link>
           <Link to="/gov-home">
-            <p className="font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
+            <p className="hidden lg:flex font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
               For Government
+            </p>
+            <p className="flex lg:hidden font-normal text-lg hover:border-gray-100 hover:border-b-2 duration-500 cursor-pointer">
+              Government
             </p>
           </Link>
         </div>
