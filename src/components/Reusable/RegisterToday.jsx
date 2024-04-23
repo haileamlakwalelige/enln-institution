eebb735646b31e8ddc31d3a7172ccfd1a8c3dac3
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-23 10:36:12 +0300

    update the register today section by removing nested link, it is not acceptable in html

--- src/components/Reusable/RegisterToday.jsx
+++ src/components/Reusable/RegisterToday.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+
 function RegisterToday() {
   return (
     <div className="bg-secondary">
@@ -9,28 +10,28 @@ function RegisterToday() {
         </h1>
         <div className="mt-6 shrink-0 md:mx-3 md:mt-0 md:w-auto">
           <Link to="/signup">
-          <motion.a
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ type: "spring", stiffness: 400, damping: 17 }}
-            className="buttontext btn btn-primary w-full rounded-lg border-none px-8 py-2 tracking-wider duration-300 lg:w-auto "
-          >
-            <span>Start Today</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-5 w-5"
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+              className="buttontext btn btn-primary w-full rounded-lg border-none px-8 py-2 tracking-wider duration-300 lg:w-auto "
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-              />
-            </svg>
-          </motion.a>
+              <span>Start Today</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </motion.div>
           </Link>
         </div>
       </div>
