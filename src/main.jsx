a2703aeff27a8767a3da3c07fce4eca64dc84c21
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-23 10:32:54 +0300

    remove react query from here

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from "react-router-dom";
 import './index.css'
-import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import store from './store/store.js';
 import { ToastContainer } from 'react-toastify';
 
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <QueryClientProvider client={queryClient}>
         <Provider store={store}>
         <ToastContainer />
           <App />
         </Provider>
-      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );
+
