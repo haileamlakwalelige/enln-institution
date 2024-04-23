40f93d60c8e11dff5bcb569171c03ab0486542d2
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-23 10:35:35 +0300

    update the price by adding redux toolkit state and use reduce method to calculate the total

--- src/screens/AddToCart.jsx
+++ src/screens/AddToCart.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import HorizontalCard from "../components/Reusable/HorizontalCard";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const AddToCart = () => {
+  const cartItems = useSelector(state => state.cart);
+
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
   return (
     <div className="pt-10 px-2 sm:px-6 md:px-12 lg:px-20">
       <div>
@@ -14,9 +19,6 @@ const AddToCart = () => {
         <div className="my-10 flex flex-wrap justify-center items-start gap-4">
           <div>
             <HorizontalCard />
-            <HorizontalCard />
-            <HorizontalCard />
-            <HorizontalCard />
           </div>
           <div className="flex justify-center items-center w-[305px] h-[241px] border-primary border-[1px] rounded-xl">
             <div className=" flex justify-center items-start  flex-col">
@@ -24,14 +26,16 @@ const AddToCart = () => {
                 Total
               </p>
               <p className="text-primary text-[20px] merb mb-4 md:text-[24px] lg:text-[28px] ">
-                Birr 4,500
+                Birr {totalPrice}
               </p>
+              <Link to="/checkout">
               <button
                 //   type="submit"
                 className="bg-primary rounded-xl text-white font-semibold text-xl py-2 px-10 max-w-[300px] min-w-[250px] my-4"
               >
                 Checkout
               </button>
+              </Link>
             </div>
           </div>
         </div>
