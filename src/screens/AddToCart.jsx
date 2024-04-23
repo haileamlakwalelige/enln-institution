2a6453b81898383d309b4f6fd7f5a7b6d01f168a
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-23 16:23:55 +0300

    update add to cart section.

--- src/screens/AddToCart.jsx
+++ src/screens/AddToCart.jsx
@@ -11,10 +11,10 @@ const AddToCart = () => {
     <div className="pt-10 px-2 sm:px-6 md:px-12 lg:px-20">
       <div>
         <p className="text-primary font-semibold merb text-[20px] md:text-[24px] lg:text-[28px] xl:text-[32px] ">
-          Add To Cart
+          Added To Cart
         </p>
         <p className="font-bold text-[14px] text-black amib sm:text-[16px] py-3">
-          4 Courses in the cart
+          {cartItems.length} Courses in the cart
         </p>
         <div className="my-10 flex flex-wrap justify-center items-start gap-4">
           <div>
