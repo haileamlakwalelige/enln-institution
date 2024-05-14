cc09b6cdbdc924f2f602157368792a139323b43c
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-14 14:38:30 +0300

    update the styles of the add to cart section

--- src/components/Reusable/HorizontalCard.jsx
+++ src/components/Reusable/HorizontalCard.jsx
@@ -22,23 +22,23 @@ const HorizontalCard = () => {
             whileTap={{ scale: 0.95 }}
           >
             <motion.div
-              className="flex h-full w-full flex-wrap overflow-hidden rounded bg-white"
+              className="flex h-full w-full flex-nowrap overflow-hidden rounded bg-white"
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5 }}
             >
               <div className="w-2/6">
                 <img
-                  className="h-64 w-full object-cover"
+                  className="h-64 min-w-[70] w-full object-cover"
                   src={item.image}
                   alt="Course"
                 />
               </div>
               <div className="md:w-4/6 space-y-1 p-5 md:space-y-2">
               <div onClick={()=>removeItem(item)} className="flex justify-end items-end mb-2 text-lg pb-5 cursor-pointer"><FaTimes className="text-red-500 font-bold text-end justify-self-end"/></div>
-                <div className="card-actions justify-between">
+                <div className="card-actions justify-between items-center flex">
                   <Link to="/course-description">
-                    <h2 className="text font-bold">{item.title}</h2>
+                    <h2 className="text font-bold text-center md:text-start">{item.title}</h2>
                   </Link>
                   
                   <h2 className="text font-bold hidden lg:block">Birr {item.price}</h2>
