889aa7d8554c219ff6161cda14196a157967cac2
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-06-06 17:49:29 +0300

    update the course hero

--- src/components/course description/CoursesHero.jsx
+++ src/components/course description/CoursesHero.jsx
@@ -4,7 +4,7 @@ import { add } from '../../store/cartSlice';
 import { Link } from 'react-router-dom';
 import back from '../../assets/back.png';
 import tom from '../../assets/tom.png';
-import { IoStarSharp } from 'react-icons/io5';
+import RatingsDisplay from '../Reusable/RatingsDisplay';
 
 const CoursesHero = ({ course }) => {
   const dispatch = useDispatch();
@@ -36,27 +36,21 @@ const CoursesHero = ({ course }) => {
                 {course.title}
               </p>
               <p className="text-primary text-[14px] lg:text-[16px] font-medium py-4">
-                Unlock the secrets to optimal health with our Complete Nutrition
+                {/* Unlock the secrets to optimal health with our Complete Nutrition
                 Course, designed for individuals seeking a holistic
-                understanding of nutrition.
-                {/* {course.description} */}
+                understanding of nutrition. */}
+                {course.description}
               </p>
             </div>
             <div className="flex gap-2 text-primary flex-wrap">
-              <p>{course.rate}.0</p>
               <div className="flex gap-1 mt-1">
-              <div className="flex items-center">
-                    {/* Render star ratings */}
-                    {[...Array(course.rate)].map((_, index) => (
-                      <IoStarSharp key={index} size={20} className="text-primary" />
-                    ))}
-                  </div>
+                <div className="flex items-center">
+                  <RatingsDisplay rating={Number(course.rate)} />
+                </div>
               </div>
-              <p>(110, 456)</p>
-              <p className="text-[#7E7E7E]">25</p>
+              <p className="text-[#7E7E7E]">{course.hour}</p>
               <p className="text-[#7E7E7E]">total hour</p>
-              <p className="text-[#7E7E7E]">All Level</p>
-              <p>1.356,345 students</p>
+              <p className="text-[#7E7E7E]">{course.level} Level</p>
             </div>
             <p className="py-5 text-primary text-[16pxx]">
               Created by {course.instructor_name}
@@ -65,19 +59,26 @@ const CoursesHero = ({ course }) => {
               <p className="merb text-black text-[18px] md:text-[20px] lg:text-[22px] text-center font-bold">
                 Birr {course.price}
               </p>
-              {isInCart(course.id) ? (
+              {course.price == 0 ? (
                 <Link
-                  to="/add-to-cart"
+                  to={`/course-line/${course.slug}`}
                   className="py-2 rounded-lg bg-primary hover:bg-white text-white px-10 mt-6 mb-2 hover:text-primary hover:border-2 hover:border-primary"
                 >
-                  Go to Cart
+                  Go to Course
+                </Link>
+              ) : isInCart(course.id) ? (
+                <Link
+                  to="/favorite"
+                  className="py-2 rounded-lg bg-primary hover:bg-white text-white px-10 mt-6 mb-2 hover:text-primary hover:border-2 hover:border-primary"
+                >
+                  Go to Favorite
                 </Link>
               ) : (
                 <button
                   onClick={() => addToCart(course)}
                   className="py-2 rounded-lg bg-primary hover:bg-white text-white px-10 mt-6 mb-2 hover:text-primary hover:border-2 hover:border-primary"
                 >
-                  Add to Cart
+                  Add to Favorite
                 </button>
               )}
             </div>
@@ -93,15 +94,22 @@ const CoursesHero = ({ course }) => {
 
 CoursesHero.propTypes = {
   course: PropTypes.shape({
-    title: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
-    price: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    audience: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    // notes: PropTypes.arrayOf(PropTypes.string).isRequired,
+    // details: PropTypes.arrayOf(PropTypes.string).isRequired,
     // image: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    level: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    // links: PropTypes.string.isRequired,
     instructor_name: PropTypes.string.isRequired,
-    rate: PropTypes.number.isRequired,
-    // ratingsCount: PropTypes.number.isRequired,
-    // hours: PropTypes.number.isRequired,
-    // difficulty: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    hour: PropTypes.number.isRequired,
+    rate: PropTypes.string.isRequired,
   }).isRequired,
 };
 
